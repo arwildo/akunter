1c7cb118be6da04c61f6a2e1b597c54316a77962
arwildo <arwildo@gmail.com>
2020-12-06 23:36:27 +0900

    cart when empty condition DONE

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -6,7 +6,6 @@ class Buy extends Component {
 
   handleClick = (id)=>{
     this.props.addToCart(id); 
-    alert(0)
   }
 
   render() {
@@ -14,7 +13,7 @@ class Buy extends Component {
     let itemList = this.props.items.map(item=>{
 
       return(
-        <div className={item.style} key={item.id}>
+        <a className={item.style} key={item.id} onClick={()=>{this.handleClick(item.id)}}>
           <div className="relative text-white px-4 pb-4 mt-4">
             <img src={item.img} alt={item.title + " " + item.desc}/>
             <span className="block opacity-75 -mb-1">{item.title}</span>
@@ -23,7 +22,7 @@ class Buy extends Component {
               <span className="block bg-white rounded-full text-purple-500 md:ml-4 mt-2 md:mt-0 text-xs font-bold px-3 py-2 leading-none flex items-center">${item.price}K</span>
             </div>
           </div>
-        </div>
+        </a>
       )
     })
 
--- frontend/src/pages/Pay.js
+++ frontend/src/pages/Pay.js
@@ -1,20 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-function Pay() {
-  return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
+class Pay extends Component{
 
-      {/*  Page content */}
-      <main className="flex-grow">
+  render(){
+            
+    let addedItems = this.props.items.length ?
+      (  
+        this.props.items.map(item=>{
+          return(
+            
+            <li className="collection-item avatar" key={item.id}>
+              <div className="item-img"> 
+                  <img src={item.img} alt={item.img} className=""/>
+              </div>
+          
+              <div className="item-desc">
+                  <span className="title">{item.title}</span>
+                  <p>{item.desc}</p>
+                  <p><b>Price: {item.price}$</b></p> 
+                  <p>
+                      <b>Quantity: {item.quantity}</b> 
+                  </p>
+                  <div className="add-remove">
+                      <Link to="/cart"><i className="material-icons">arrow_drop_up</i></Link>
+                      <Link to="/cart"><i className="material-icons">arrow_drop_down</i></Link>
+                  </div>
+                  <button className="waves-effect waves-light btn pink remove">Remove</button>
+              </div>
+              
+            </li>                        
+          )
+        })
+      ):
 
-        {/*  Page sections */}
-        <h1>Pay</h1>
-        
-      </main>
-
-      {/*  Site footer */}
-    </div>
-  );
+      (
+        <div className="text-gray-300 bg-red-300 h-screen"> 
+          <div className="text-gray-300 bg-blue-300 p-auto"> 
+            <p className="block h-8 text-7xl leading-8 text-center align-middle">
+              <i className="mdi mdi-cart-outline"></i>
+            </p>
+            <p className="block text-sm m-6 leading-none text-center align-middle">Empty</p>
+          </div>
+        </div>
+      )
+    return(
+    <>
+      {/*
+      <div className="container">
+        <div className="cart">
+          <ul className="collection">
+            {addedItems}
+          </ul>
+        </div>  
+      </div>
+      */}
+      <div className="flex h-screen justify-center items-center">
+        <div className="text-center text-gray-300">
+          <p className="block h-8 text-7xl leading-8 text-center align-middle">
+            <i className="mdi mdi-cart-outline"></i>
+          </p>
+          <p className="block text-sm m-6 leading-none text-center align-middle">Empty</p>
+        </div>
+      </div>
+    </>
+    )
+  }
 }
 
-export default Pay;
+const mapStateToProps = (state)=>{
+  return{
+      items: state.addedItems
+  }
+}
+
+export default connect(mapStateToProps)(Pay)
