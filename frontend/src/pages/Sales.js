61f98e884a5fd5bf82f2515aa79575249f3bfa7a
arwildo <arwildo@gmail.com>
2020-12-08 22:39:47 +0900

    build sales early design

--- frontend/src/pages/Sales.js
+++ frontend/src/pages/Sales.js
@@ -1,20 +1,120 @@
 import React from 'react';
 
-function Home() {
+function Sales() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
+    <div className="antialiased">
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div className="my-2 flex sm:flex-row flex-col">
+            <div className="flex flex-row mb-1 sm:mb-0">
+              <div className="relative">
+                <select
+                  className="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                  <option>5</option>
+                  <option>10</option>
+                  <option>20</option>
+                </select>
+                <div
+                  className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                  </svg>
+                </div>
+              </div>
 
-      {/*  Page content */}
-      <main className="flex-grow">
+              <div className="relative">
+                <select
+                  className="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
+                  <option>January</option>
+                  <option>February</option>
+                  <option>March</option>
+                </select>
+                  <div
+                    className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                  </div>
+              </div>
+            </div>
 
-        {/*  Page sections */}
-        <h1>Sales</h1>
+        </div>
 
-      </main>
-
-      {/*  Site footer */}
+        <div className="-mx-4 px-2 sm:px-8 py-2 overflow-x-auto">
+          <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
+                  <thead>
+                      <tr>
+                          <th
+                              className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                              Item
+                          </th>
+                          <th
+                              className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                              Q
+                          </th>
+                          <th
+                              className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                              Price
+                          </th>
+                          <th
+                              className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                              Time
+                          </th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                              <div className="flex items-center">
+                                  <div className="ml-3">
+                                      <p className="text-gray-900 whitespace-no-wrap">
+                                          Product Name
+                                      </p>
+                                  </div>
+                              </div>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                              <p className="text-gray-900 whitespace-no-wrap">2</p>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                              <span
+                                  className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                  <span aria-hidden
+                                      className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                  <span className="relative">$82K</span>
+                              </span>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              08-11-20
+                            </p>
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+              <div
+                  className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between">
+                  <span className="text-xs xs:text-sm text-gray-900">
+                      1 to 4 of 50 Data
+                  </span>
+                  <div className="inline-flex mt-2 xs:mt-0">
+                      <button
+                          className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                          Prev
+                      </button>
+                      <button
+                          className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+                          Next
+                      </button>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Home;
+export default Sales;
--- frontend/src/partials/Recipe.js
+++ frontend/src/partials/Recipe.js
@@ -6,7 +6,12 @@ class Recipe extends Component{
   render(){
     return(
       <div className="container">
-        <button className="text-base mx-auto mt-12 mb-28 bg-blue-500 text-white hover:bg-indigo-500 flex justify-center px-4 py-2 rounded-full font-bold cursor-pointer transform transition-all duration-300 scale-100 hover:scale-95">Checkout ${this.props.total}K</button>
+        <button 
+          className="text-base mx-auto mt-12 mb-28 bg-blue-500 text-white hover:bg-indigo-500 flex justify-center px-4 py-2 rounded-full font-bold cursor-pointer transform transition-all duration-300 scale-100 hover:scale-95"
+          onClick={() => console.log(this.props.addedItems)}
+        >
+          Checkout ${this.props.total}K
+        </button>
       </div>
     )
   }
