f92730936fd714c39d3d8c1643e17b34cf0b273b
arwildo <arwildo@gmail.com>
2020-12-14 10:17:00 +0900

    change chart

--- frontend/src/pages/Store.js
+++ frontend/src/pages/Store.js
@@ -1,27 +1,129 @@
-import React, { useRef } from 'react';
-import { LineChart } from '@opd/g2plot-react';
+import React from 'react';
+import { Chart, LineAdvance } from 'bizcharts';
 
 function Store() {
-  const config: LineChartProps = {
-    height: 350,
-    autoFit: true,
-    xField: 'year',
-    yField: 'value',
-    smooth: true,
-    data: [
-      { year: '1991', value: 3 },
-      { year: '1992', value: 4 },
-      { year: '1993', value: 3.5 },
-      { year: '1994', value: 5 },
-      { year: '1995', value: 4.9 },
-      { year: '1996', value: 6 },
-      { year: '1997', value: 7 },
-      { year: '1998', value: 9 },
-      { year: '1999', value: 11 },
-    ],
-  }
-
-  const chartRef = useRef();
+  const data = [
+    {
+      month: "Jan",
+      city: "Tokyo",
+      temperature: 7
+    },
+    {
+      month: "Jan",
+      city: "London",
+      temperature: 3.9
+    },
+    {
+      month: "Feb",
+      city: "Tokyo",
+      temperature: 13
+    },
+    {
+      month: "Feb",
+      city: "London",
+      temperature: 4.2
+    },
+    {
+      month: "Mar",
+      city: "Tokyo",
+      temperature: 16.5
+    },
+    {
+      month: "Mar",
+      city: "London",
+      temperature: 5.7
+    },
+    {
+      month: "Apr",
+      city: "Tokyo",
+      temperature: 14.5
+    },
+    {
+      month: "Apr",
+      city: "London",
+      temperature: 8.5
+    },
+    {
+      month: "May",
+      city: "Tokyo",
+      temperature: 10
+    },
+    {
+      month: "May",
+      city: "London",
+      temperature: 11.9
+    },
+    {
+      month: "Jun",
+      city: "Tokyo",
+      temperature: 7.5
+    },
+    {
+      month: "Jun",
+      city: "London",
+      temperature: 15.2
+    },
+    {
+      month: "Jul",
+      city: "Tokyo",
+      temperature: 9.2
+    },
+    {
+      month: "Jul",
+      city: "London",
+      temperature: 17
+    },
+    {
+      month: "Aug",
+      city: "Tokyo",
+      temperature: 14.5
+    },
+    {
+      month: "Aug",
+      city: "London",
+      temperature: 16.6
+    },
+    {
+      month: "Sep",
+      city: "Tokyo",
+      temperature: 9.3
+    },
+    {
+      month: "Sep",
+      city: "London",
+      temperature: 14.2
+    },
+    {
+      month: "Oct",
+      city: "Tokyo",
+      temperature: 8.3
+    },
+    {
+      month: "Oct",
+      city: "London",
+      temperature: 10.3
+    },
+    {
+      month: "Nov",
+      city: "Tokyo",
+      temperature: 8.9
+    },
+    {
+      month: "Nov",
+      city: "London",
+      temperature: 5.6
+    },
+    {
+      month: "Dec",
+      city: "Tokyo",
+      temperature: 5.6
+    },
+    {
+      month: "Dec",
+      city: "London",
+      temperature: 9.8
+    }
+  ];
 
   return (
     <div className="flex max-w-5xl mx-auto items-center min-h-screen text-gray-800">
@@ -52,10 +154,23 @@ function Store() {
             </div>
           </div>
 
-          <div className="col-span-12 align-center item-center text-center">
+          <div className="col-span-12 sm:col-span-6">
             <div className="flex flex-row bg-white shadow-sm rounded p-4">
 
-              <LineChart {...config} chartRef={chartRef} />
+              <div className="rounded overflow-hidden w-full md:flex">
+                <div className="flex w-full bg-white items-center">
+                  <Chart padding={[10, 20, 50, 40]} autoFit height={300} data={data} >
+                      <LineAdvance
+                        shape="smooth"
+                        point
+                        area
+                        position="month*temperature"
+                        color="city"
+                      />
+                    </Chart>
+                </div>
+              </div>
+
 
             </div>
           </div>
