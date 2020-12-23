ceb2ecdcd66d44ae77b0c0622f4a0a39b5c4a488
arwildo <arwildo@gmail.com>
2020-12-23 14:18:57 +0900

    Updates

---
+++ precache-manifest.1fa8baf53a0856a2760b6543f6d5b6a0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1266f00f1f6f08f425186319b603946",
+    "url": "/akunter/index.html"
+  },
+  {
+    "revision": "c545bf5edb2551a87d9b",
+    "url": "/akunter/static/css/main.35b7494d.chunk.css"
+  },
+  {
+    "revision": "be317469406c4269fd8e",
+    "url": "/akunter/static/js/2.b082387c.chunk.js"
+  },
+  {
+    "revision": "f17edcf435a96e03323e79bde7e7e5ef",
+    "url": "/akunter/static/js/2.b082387c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c545bf5edb2551a87d9b",
+    "url": "/akunter/static/js/main.d7dc8dec.chunk.js"
+  },
+  {
+    "revision": "910710dd862e19fe4379",
+    "url": "/akunter/static/js/runtime-main.75da1dbe.js"
+  },
+  {
+    "revision": "6243de05c9fc683d5794a9179ac998d7",
+    "url": "/akunter/static/media/beef-chicken-deluxe.6243de05.png"
+  },
+  {
+    "revision": "fdf5c831b70183d932ccba3df72362b3",
+    "url": "/akunter/static/media/beef-chicken-original.fdf5c831.png"
+  },
+  {
+    "revision": "8ceb548e580f0c0be713356f2bf2c16c",
+    "url": "/akunter/static/media/beef-madness.8ceb548e.png"
+  },
+  {
+    "revision": "1511c00d9cfaaefce3127d049788728d",
+    "url": "/akunter/static/media/cashier.1511c00d.jpg"
+  },
+  {
+    "revision": "a46bc6edc11762b6bac00ef6fde5f386",
+    "url": "/akunter/static/media/chicken-crispy.a46bc6ed.png"
+  },
+  {
+    "revision": "7b6b03d878ecf6e625a70d90e154b565",
+    "url": "/akunter/static/media/sales.7b6b03d8.jpg"
+  },
+  {
+    "revision": "74504c1c828543068a22ae80574d1917",
+    "url": "/akunter/static/media/store.74504c1c.jpg"
+  }
+]);
