f18916649284343f90cc33be0a04e2f49952cb6d
arwildo <arwildo@gmail.com>
2020-12-23 11:16:10 +0900

    implement alert on success order

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,32 +4,96 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
-import { Provider as AlertProvider } from 'react-alert';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
 
 import App from './App';
 import cartReducer from './reducers/cartReducer';
 
+// Alert settings
 const AlertTemplate = ({ style, options, message, close }) => (
   <div style={style}>
-    {options.type === 'info' && '!'}
-    {options.type === 'success' && ':)'}
-    {options.type === 'error' && ':('}
-    {message}
-    <button onClick={close}>X</button>
+    {options.type === 'info' &&
+      <section className="flex items-center justify-center">
+        <div className="flex items-center bg-white shadow rounded-md px-2 mx-8" style={{width: "15rem"}}>
+          <div className="mr-6 bg-blue-500 rounded px-4 py-2 text-center -ml-3">
+            <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 text-white" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+            </svg>
+          </div>
+          <div className="flex items-center">
+            <h2 className="text-blue-500 text-lg font-bold mr-2 ">{message}</h2>
+          </div>
+          <button className="flex justify-end flex-1" onClick={close}>
+            <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 text-red-600" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
+            </svg>
+          </button>
+        </div>
+      </section>
+    }
+
+    {options.type === 'success' &&
+      <section className="flex items-center justify-center">
+        <div className="flex items-center bg-white shadow rounded-md px-2 mx-8" style={{width: "15rem"}}>
+          <div className="mr-6 bg-green-500 rounded px-4 py-2 text-center -ml-3">
+            <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 text-white" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"/>
+            </svg>
+          </div>
+          <div className="flex items-center">
+            <h2 className="text-green-500 text-lg font-bold mr-2 ">{message}</h2>
+          </div>
+          <button className="flex justify-end flex-1" onClick={close}>
+            <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 text-red-600" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
+            </svg>
+          </button>
+        </div>
+      </section>
+    }
+
+    {options.type === 'error' && 
+      <section className="flex items-center justify-center">
+        <div className="flex items-center bg-white shadow rounded-md px-2 mx-8" style={{width: "15rem"}}>
+          <div className="mr-6 bg-red-500 rounded px-4 py-2 text-center -ml-3">
+            <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 text-white" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"/>
+            </svg>
+          </div>
+          <div className="flex items-center">
+            <h2 className="text-red-500 text-lg font-bold mr-2 ">{message}</h2>
+          </div>
+          <button className="flex justify-end flex-1" onClick={close}>
+            <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 text-red-600" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
+            </svg>
+          </button>
+        </div>
+      </section>
+    }
+
+
   </div>
 )
+const options = {
+  position: positions.MIDDLE,
+  timeout: 3000,
+  offset: '10px',
+  transition: transitions.SCALE
+}
 
+// Redux
 const store = createStore(cartReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <AlertProvider template={AlertTemplate}>
-      <Provider store={store}>
-        <Router>
+    <Provider store={store}>
+      <Router>
+        <AlertProvider template={AlertTemplate} {...options}>
           <App />
-        </Router>
-      </Provider>
-    </AlertProvider>
+        </AlertProvider>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- frontend/src/partials/Recipe.js
+++ frontend/src/partials/Recipe.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { useAlert } from 'react-alert';
 
 import { removeItem } from '../actions/cartActions';
 
-class Recipe extends Component {
+function Recipe(props) {
   // Remove the item completely
-  handleRemove = (id) => {
-    this.props.removeItem(id);
+  const handleRemove = (id) => {
+    props.removeItem(id);
   }
 
-  saveData = (data) => {
+  const saveData = (data) => {
     const d = new Date(),
       dformat = [d.getDate(),
                 d.getMonth()+1,
@@ -23,27 +23,28 @@ class Recipe extends Component {
 
     axios
       .post("/api/akunters/", activeData);
-    this.handleRemove(data.id);
+    handleRemove(data.id);
   }
 
-  runSaveData = () => {
-    this.props.addedItems.map(this.saveData);
-    useAlert().show("Success wow");
+  const alert = useAlert();
+
+  const runSaveData = (total) => {
+    props.addedItems.map(saveData);
+    alert.success('$' + total +'K');
   }
 
-  render(){
-    return(
-      <div className="container">
-        <button 
-          className="text-base mx-auto mt-12 mb-28 bg-blue-500 text-white hover:bg-indigo-500 flex justify-center px-4 py-2 rounded-full font-bold cursor-pointer transform transition-all duration-300 scale-100 hover:scale-95"
-          onClick={() => this.runSaveData()}
-        >
-          Checkout $ {this.props.total}K
-        </button>
-      </div>
-    )
-  }
+  return(
+    <div className="container">
+      <button 
+        className="text-base mx-auto mt-12 mb-28 bg-blue-500 text-white hover:bg-indigo-500 flex justify-center px-4 py-2 rounded-full font-bold cursor-pointer transform transition-all duration-300 scale-100 hover:scale-95"
+        onClick={() => runSaveData(props.total)}
+      >
+        Checkout $ {props.total}K
+      </button>
+    </div>
+  )
 }
+
 
 const mapStateToProps = (state)=>{
   return {
@@ -57,5 +58,4 @@ const mapDispatchToProps = (dispatch)=>{
   }
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Recipe)
+export default connect(mapStateToProps, mapDispatchToProps) (Recipe)
