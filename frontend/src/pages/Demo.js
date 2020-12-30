14375f0d74d6270cd41ba135fc2913b980e5257b
arwildo <arwildo@gmail.com>
2020-12-30 21:00:53 +0900

    set up mocker-data-generator

--- frontend/src/pages/Demo.js
+++ frontend/src/pages/Demo.js
@@ -1,30 +1,13 @@
-import db from './db.json';
+import mocker from 'mocker-data-generator';
 
-let last30Days;
-let dates = []
-let dateNow = new Date(Date.now() + (3600 * 1000 * 24 * 2));
+var demoData = {
+    id: {
+        chance: 'guid'
+    },
+    item: {
+        faker: 'random.number({"min": 1, "max": 6})'
+    },
+};
 
-// Get today last 30 days date
-const get30DaysDate = () => {
-  for (let day=31; day>1; day--) {
-    last30Days = new Date(dateNow - day * 24 * 60 * 60 * 1000).toISOString();
-    let clean = last30Days.split(":")[0];
-    clean = clean.split("T")[0];
-    clean = clean.split("-").reverse();
-    clean = clean.join("-");
-    dates.push(clean);
-  }
-}
-get30DaysDate();
-
-
-const demoData = () => {
-  for (let i in dates) {
-    console.log("one " + db[i].time.split(' ')[0] + " and two " + dates[i]);
-    db[i].time.split(' ')[0] = dates[i]
-  }
-
-  return db;
-}
-
-export default demoData;
+mocker()
+    .schema('cats', demoData, 3)
