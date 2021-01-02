106c2c8279444fe1a3ce4a142c0124e48066d395
arwildo <arwildo@gmail.com>
2021-01-02 23:42:52 +0900

    add quantity key/value

--- frontend/src/pages/Demo.js
+++ frontend/src/pages/Demo.js
@@ -1,10 +1,14 @@
 let demoData = [];
 
+const numArr = [1, 2, 3, 4, 5, 6];
+
 for (let i=0; i<100; i++) {
   let itemData = {}
 
   itemData.id = i;
+  itemData.item = numArr[Math.floor(Math.random() * numArr.length)];
   itemData.time = i + 2;
+  itemData.quantity = numArr[Math.floor(Math.random() * numArr.length)]; 
 
   demoData.push(itemData);
 }
