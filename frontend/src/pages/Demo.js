59b5ee00a8ad46bbc3e9ce208751b99e7cd694bf
arwildo <arwildo@gmail.com>
2021-01-03 21:25:40 +0900

    add dates and time to time key/value

--- frontend/src/pages/Demo.js
+++ frontend/src/pages/Demo.js
@@ -1,13 +1,28 @@
 let demoData = [];
 
 const numArr = [1, 2, 3, 4, 5, 6];
+const timeArr = ["15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56"];
+let last30Days;
+let dates = []
+let dateNow = new Date(Date.now() + (3600 * 1000 * 24 * 2));
 
-for (let i=0; i<100; i++) {
+// Get all the days
+for (let day=31; day>1; day--) {
+  last30Days = new Date(dateNow - day * 24 * 60 * 60 * 1000).toISOString();
+  let clean = last30Days.split(":")[0];
+  clean = clean.split("T")[0];
+  clean = clean.split("-").reverse();
+  clean = clean.join("-");
+  dates.push(clean);
+}
+
+// Main
+for (let i=0; i<30; i++) {
   let itemData = {}
 
   itemData.id = i;
   itemData.item = numArr[Math.floor(Math.random() * numArr.length)];
-  itemData.time = i + 2;
+  itemData.time = dates[i] + " " + timeArr[i]
   itemData.quantity = numArr[Math.floor(Math.random() * numArr.length)]; 
 
   demoData.push(itemData);
