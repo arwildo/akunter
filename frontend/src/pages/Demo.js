b92b04ba0090e02402b5b230ba1b55d51023284d
arwildo <arwildo@gmail.com>
2021-01-03 21:45:29 +0900

    clean way to multiply timeArr

--- frontend/src/pages/Demo.js
+++ frontend/src/pages/Demo.js
@@ -1,13 +1,22 @@
-let demoData = [];
-
 const numArr = [1, 2, 3, 4, 5, 6];
-const timeArr = ["15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56", "15:13", "15:45", "16:24", "17:41", "18:08", "20:18", "21:56"];
 let last30Days;
 let dates = []
+let demoData = [];
 let dateNow = new Date(Date.now() + (3600 * 1000 * 24 * 2));
 
-// Get all the days
-for (let day=31; day>1; day--) {
+// Repeat array
+const arrMult = (arr, n) => {
+  var a = [];
+  for (var i=0;i<n;[i++].push.apply(a,arr));
+  return a;
+}
+
+let timeArr = ["15:13", "15:45", "16:24", "17:41", "18:9", "20:18", "21:56"];
+timeArr = arrMult(timeArr, 15);
+
+
+// Get all the last30days's days
+for (let day=94; day>1; day--) {
   last30Days = new Date(dateNow - day * 24 * 60 * 60 * 1000).toISOString();
   let clean = last30Days.split(":")[0];
   clean = clean.split("T")[0];
@@ -17,7 +26,7 @@ for (let day=31; day>1; day--) {
 }
 
 // Main
-for (let i=0; i<30; i++) {
+for (let i=0; i<93; i++) {
   let itemData = {}
 
   itemData.id = i;
